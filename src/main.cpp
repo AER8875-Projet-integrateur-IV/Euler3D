97d0d3a2fb14b2d9ea65f7fbfd9ec690597e2d8f
rouga <rouga2012@hotmail.fr>
2021-01-11 14:58:52 -0500

    docs and Cmake

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * @brief Entry point for the Euler3D software
+ * @param argc : Number of command line parameters
+ * @param argv : command line parameters in list of characters
+ * @return State of the program: 0 -> all good otherwise an error is thrown (1,2,3....)
+*/
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Euler 3D software." << std::endl;
+    return 0;
+}
