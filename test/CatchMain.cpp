28c2d838eccdbe0aa58791db6329e88e85a89b2b
abraham <abraham.bherer-constant@polymtl.ca>
2021-01-12 10:53:36 -0500

    Implement basic testing framework with Catch2
    The source code for Catch2 v2.13.4 is currently included in the repo.
    This is simpler for new users since they do not have to handle git
    submodules.
    
    The header file only approach has not been used since it is
    unrecommended by the Catch2 documentation.
    
    V2 is used since the documentation is incomplete for V3.

---
+++ test/CatchMain.cpp
@@ -0,0 +1,12 @@
+
+/**
+ * @file CatchMain.cpp
+ * @author Abraham Bherer-Constant
+ * @date 2021/01/12
+ * @brief Make use of Catch2 default main definition
+ *
+ * This file should not contain any tests
+ */
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
---
+++ test/Test_Catch.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * @file Test_Catch.cpp
+ * @author Abraham Bherer-Constant
+ * @date 2021/01/12
+ * @brief Trivial test to check that Catch is correctly working
+ *
+ * This file should not contain any other tests, but it can be
+ * used as a template for new test files
+ */
+#include <catch2/catch.hpp>
+
+TEST_CASE("Catch is correctly working") { REQUIRE(1 == 1); }
