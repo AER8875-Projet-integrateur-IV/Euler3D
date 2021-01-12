5fb8723ebce0417b6c8aa34ac909b437ce1948e8
abraham <abraham.bherer-constant@polymtl.ca>
2021-01-12 16:53:45 -0500

    Add comments to header

--- include/Partition.hpp
+++ include/Partition.hpp
@@ -1,3 +1,15 @@
+
+/**
+ * @file Partition.hpp
+ * @author Aziz Ndiaye, Abraham Bherer-Constant
+ * @date 2021/01/12
+ * @brief Partition a mesh object into multiple smaller mesh objects 
+ *
+ * Partition a mesh on which connectivity has already been solved with
+ * the METIS library into multiple smaller mesh and create a vector of
+ * mesh objects with solved connectivity 
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,26 +24,37 @@ class Marker
 class Partition
 {
 private:
-    Mesh *_m_meshGlobal;
+    Mesh *_m_meshGlobal; //   Mesh object to be partitionned, connectivity must already be solved  
 
-    int _m_nPart; // Nombre de partitions
-    std::vector<int> _m_elem2Part;
-    std::vector<int> _m_Part2Elem;
-    std::vector<int> _m_Part2ElemStart;
+    int _m_nPart; //   Nombre de partitions  
+    std::vector<int> _m_elem2Part; //   i position holds the partition number for global element number i (0 based) 
+    std::vector<int> _m_Part2Elem; //   contain the global element indexes for each partition in continuous blocks  
+    std::vector<int> _m_Part2ElemStart; //   contain the start index of each block associated to a partition in _m_Part2Elem  
 
-    std::vector<int> _m_nElemPerPart;
-    std::vector<int> _m_nNodePerPart;
+    std::vector<int> _m_nElemPerPart; //   number of elements in each partition  
+    std::vector<int> _m_nNodePerPart; //   number of nodes in each partition  
 
-    std::vector<int> _m_localNode2Global;
+    // linked list to go from local to globalindex
+    // _m_localNode2Global contains the global index for local node i at position i for each partition in continuous blocks
+    // _m_localNode2GlobalStart contains the start index for each partition in _m_localNode2Global
+    std::vector<int> _m_localNode2Global; 
     std::vector<int> _m_localNode2GlobalStart;
 
+    // same as _m_localNode2Global and _m_localNode2GlobalStart
     std::vector<int> _m_localElem2Global;
     std::vector<int> _m_localElem2GlobalStart;
 
-    std::vector<Mesh> _m_part;
+    std::vector<Mesh> _m_part;  
     std::vector<Marker> _m_marker;
 
 public:
+    /** 
+     * @brief       Constructor 
+     *
+     * @param[in]   meshGlobal      mesh to be partitionned with solved connectivity
+     * @param[in]   nPart           number of mesh partitions required
+     *
+     */ 
     Partition(Mesh *meshGlobal, int &nPart);
 
     void Write();
