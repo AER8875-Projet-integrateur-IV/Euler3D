a21331c5194037398a47d3c452d35afa175b19fd
olivierlevesque96 <olivier.levesque96@gmail.com>
2021-01-12 13:53:04 -0500

    test

---
+++ src/Connectivity/Connectivity.hpp
@@ -0,0 +1,25 @@
+
+struct Coord
+{
+    double x;
+    double y;
+    double z;
+};
+
+
+
+
+class MeshConnectivity {
+    // Creates the connectivity from the MeshReader classes
+protected:
+        int nDim;
+        int nNode;
+        int nElem;
+        int nFace;
+        std::vector<element> elem;
+        std::vector<border> marker;
+
+public:
+
+
+};
