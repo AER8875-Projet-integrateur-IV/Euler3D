6b13450cc8186c4beb8c7a1226dcf9cc6d81fd00
charles D <charles19982009@hotmail.com>
2021-01-26 16:31:03 -0500

    started element2element2

--- src/Connectivity/exemple.cpp
+++ src/Connectivity/exemple.cpp
@@ -39,6 +39,8 @@ int main(){
   element2elementStart.resize(nElem+1);
 
 
+  //node2element
+
   int startI;
   int endI;
   int nodeI;
@@ -79,6 +81,8 @@ int main(){
     printf("%2d ", node2element[i] );
   }
 
+  //element2element
+
 
   int nlocalfaces[4] = {5,6,5,6};
   std::cout << '\n' <<element2faceStart[0] << " ";
@@ -97,6 +101,114 @@ int main(){
   std::vector<int> element2element(element2elementStart[nElem],-1);
   std::vector<int> element2face(element2elementStart[nElem],-1);
   std::vector<int> face2element(nFaces*2,-1);
+
+
+
+
+  int VTK[nElem] = {14,12,13,12};
+  std::vector<int> nFacefElement;
+  std::vector<std::vector<int>> nNodefFace;
+  nFacefElement.resize(nElem);
+  nNodefFace.resize(nElem);
+
+  for (size_t i = 0; i < nElem; i++) {
+    if (VTK[i] == 10) {
+      nFacefElement[i] = 4;
+      nNodefFace[i].resize(nFacefElement[i]);
+      nNodefFace[i][0] = 3;
+      nNodefFace[i][1] = 3;
+      nNodefFace[i][2] = 3;
+      nNodefFace[i][3] = 3;
+
+    }
+    else if (VTK[i] == 12) {
+      nFacefElement[i] = 6;
+      nNodefFace[i].resize(nFacefElement[i]);
+      nNodefFace[i][0] = 4;
+      nNodefFace[i][1] = 4;
+      nNodefFace[i][2] = 4;
+      nNodefFace[i][3] = 4;
+      nNodefFace[i][4] = 4;
+      nNodefFace[i][5] = 4;
+
+    }
+    else if (VTK[i] == 13) {
+      nFacefElement[i] = 5;
+      nNodefFace[i].resize(nFacefElement[i]);
+      nNodefFace[i][0] = 3;
+      nNodefFace[i][1] = 4;
+      nNodefFace[i][2] = 4;
+      nNodefFace[i][3] = 4;
+      nNodefFace[i][4] = 3;
+    }
+    else if (VTK[i] == 14) {
+      nFacefElement[i] = 5;
+      nNodefFace[i].resize(nFacefElement[i]);
+      nNodefFace[i][0] = 4;
+      nNodefFace[i][1] = 3;
+      nNodefFace[i][2] = 3;
+      nNodefFace[i][3] = 3;
+      nNodefFace[i][4] = 3;
+    }
+  }
+
+
+
+
+  for (size_t ielem = 0; ielem < nElem; ielem++) {
+    startI = element2nodeStart[ielem];
+    endI = element2nodeStart[ielem+1];
+    if (VTK[ielem] == 12) {
+      //methode pour cube
+      int nLocalFacefElement = nFacefElement[ielem];
+
+      int lhelp_indice[24] = {0,3,2,1,0,1,5,4,1,2,6,5,2,3,7,6,3,0,4,7,4,5,6,7};
+      int lhelp_indiceStart[nLocalFacefElement] = {0,4,8,12,16,20};
+      for (size_t iface = 0; iface < nLocalFacefElement; iface++) {
+        int nLocalNodefFace = nNodefFace[ielem][iface];
+        int lhelp[nLocalNodefFace[iface]] = {0};
+        int lpoint[nNode] = {0};
+        for (size_t inode = 0; inode < nLocalNodefFace[iface]; inode++) {
+          lhelp[inode] = element2node[startI+lhelp_indice[lhelp_indiceStart[iface]+inode]];
+          lpoint[lhelp[inode]]  = 1;
+        }
+        int ipoint = lhelp[0];
+
+        int elemStart = node2elementStart[ipoint];
+        int elemEnd = node2elementStart[ipoin+1];
+
+        for (size_t j = elemStart; j < elemEnd; j++) {
+          jelem = node2element[j];
+
+          if (jelem != ielem) {
+            startJ = element2nodeStart[jelem];
+            endJ = element2nodeStart[jelem+1];
+
+
+          }
+        }
+
+      }
+
+
+    }
+    else if (VTK[ielem] == 13) {
+      //methode pour wedge
+
+    }
+    else if (VTK[ielem] == 14) {
+      //methode pour pyramide base carree
+
+    }
+    else if (VTK[ielem] == 10) {
+      //methode pour tetrahedre
+
+    }
+    else {
+      std::cout << "VTK not recognised!" << '\n';
+    }
+  }
+
   //verifier l'ordre des faces pour chaque element, ex. dans une pyramide, la base est-elle toujours a l'indice 0?
 
 }
