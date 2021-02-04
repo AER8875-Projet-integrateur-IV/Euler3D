cbdaaf9febfa9de5da7d028467447e8215ee76ef
Amin <rouga2012@hotmail.fr>
2021-02-04 12:05:13 -0500

    Added some commments

--- include/parser/SimConfig.hpp
+++ include/parser/SimConfig.hpp
@@ -33,37 +33,75 @@ namespace E3D::Parser {
     public:
 
 
-
+        /**
+         * @return Vector of strings of mesh file names
+         */
         [[nodiscard]] inline const std::vector<std::string>& getPartitionedMeshFiles() const { return _meshFiles; }
 
+        /**
+         * @return tecplot output file path (relative to executable) and name
+         */
         [[nodiscard]] inline const std::string& getTecplotFile() const                       { return _tecplotFile; }
 
+        /**
+         * @return Angle of attack in degrees
+         */
         inline double getAoA() const                                                         { return _aoa; }
 
+        /**
+         * @return Mach number of simulation
+         */
         inline double getMach() const                                                        { return _mach; }
 
+        /**
+         * @return Velocity in m/s
+         */
         inline double getVelocity() const                                                    { return _velocity;}
 
+        /**
+         * @return  pressure in Pa
+         */
         inline double getPressure() const                                                    { return _pressure; }
 
+        /**
+         * @return  Temperature in K
+         */
         inline double getTemperature() const                                                 { return _temp;}
 
+        /**
+         * @return  Viscosity in Ns/m^2
+         */
         inline double getViscosity() const                                                   { return _visc; }
 
+        /**
+         * @return  Density in kg/m^3
+         */
         inline double getDensity() const                                                     { return _density; }
 
         inline double getGamma() const                                                       { return _gamma; }
 
+        /**
+         * @return Specific heat in J/Kg.k
+         */
         inline double getSpecificHeat() const                                                { return _specificHeat; }
 
+        /**
+         * @return  Gas constant in J/kg.K
+         */
         inline double getGasConstant() const                                                 { return _gasConstant; }
 
         inline double getCFL() const                                                         { return _cfl; }
 
         inline double getMinResidual() const                                                 { return _minResidual; }
 
+        /**
+         * @return  Type of Solver Scheme (ROE or AUSM)
+         */
         inline SolverScheme getSolverScheme() const                                          { return _solverScheme; }
 
+        /**
+         * @return  Type of Temporal Scheme (EXPLICIT_EULER , IMPLICIT_EULER or RK5)
+         */
         inline TemporalScheme getTemporalScheme() const                                      { return _temporalScheme;}
 
         inline int getMaxNumberIterations() const                                            { return _maxIter; }
@@ -95,7 +133,6 @@ namespace E3D::Parser {
         SpeedType _spdoption;
         int _maxIter;
         int _nbPartition;
-
 
 
     };
