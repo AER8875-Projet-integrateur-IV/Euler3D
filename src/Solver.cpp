76dfa9ac2c03152e7a59b0ac6e47de3df06859c0
Amin <rouga2012@hotmail.fr>
2021-02-04 11:50:49 -0500

    split executables into Pre and Solver

---
+++ src/Solver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "parser/SimConfig.hpp"
+int main(int argc, char* argv[]){
+
+    std::cout << "\n\nEuler 3D Solver." << std::endl;
+
+    if (argc != 2) {
+        std::cerr << "Usage : E3D_Solver <configFile.e3d> " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::string configFile = argv[1];
+    E3D::Parser::SimConfig config(configFile);
+    std::cout << config.getNumberPartitions();
+    return 0;
+}
