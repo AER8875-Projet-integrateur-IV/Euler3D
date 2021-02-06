278eff85a1da5f5dd80f50a61e43050965e05f18
Amin <rouga2012@hotmail.fr>
2021-02-05 19:28:39 -0500

    MPI code to initialize solver

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,5 +1,12 @@
+
 #include <iostream>
+#include <mpi.h>
+#include <cstdlib>
+
+#include "parser/SU2PartitionParser.hpp"
 #include "parser/SimConfig.hpp"
+
+using namespace E3D;
 int main(int argc, char* argv[]){
 
     std::cout << "\n\nEuler 3D Solver." << std::endl;
@@ -9,7 +16,34 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 
+
+
+
+
+    int rankID,nb_processes;
+
+    // Initialize MPI code
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rankID);
+    MPI_Comm_size(MPI_COMM_WORLD,&nb_processes);
+
+    // Parsing Config file
     std::string configFile = argv[1];
     E3D::Parser::SimConfig config(configFile);
+
+    // Check if MPI PROCESSES = Nb of partitions
+    if (rankID == 0){
+        if (config.getNumberPartitions() != nb_processes){
+            printf("Number of mesh files and MPI processes are not equal !\n");
+            printf("Number of partition = %i | Number of MPI processes : %i \n", config.getNumberPartitions(), nb_processes);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+//Parser::SU2PartitionParser rankPartitionParser(config.getPartitionedMeshFiles()[rankID], rankID);
+
+    MPI_Finalize();
+
+
     return 0;
 }
