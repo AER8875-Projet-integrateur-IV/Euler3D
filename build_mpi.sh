29127c748a3b63ac4f5c201266deaead593aedee
Amin <rouga2012@hotmail.fr>
2021-02-16 17:55:01 -0500

    add permissions v7

--- build_mpi.sh
+++ build_mpi.sh
@@ -1,18 +1,26 @@
-if [ -f "openmpi/bin/mpirun" ] && [ -f "openmpi-4.0.1/config.log" ]; then
-	echo "Using cached OpenMPI"
-	echo "Configuring OpenMPI"
-	cd openmpi-4.0.1
-	./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
-    else
-        # install OpenMPI from source
-	echo "Downloading OpenMPI Source"
-	wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz
-	tar zxf openmpi-4.0.1.tar.gz
-	echo "Configuring and building OpenMPI"
-	cd openmpi-4.0.1
-	./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
-	make -j4 &> openmpi.make
-	make install &> openmpi.install
-	cd ..
+#!/bin/sh
 
+if [ -f mpich/lib/libmpich.so ]; then
+  echo "libmpich.so found -- nothing to build."
+else
+  echo "Downloading mpich source."
+  wget http://www.mpich.org/static/downloads/3.4.1/mpich-3.4.1.tar.gz
+  tar xfz mpich-3.4.1.tar.gz
+  rm mpich-3.4.1.tar.gz
+  echo "configuring and building mpich."
+  cd mpich-3.4.1
+  ./configure \
+          --prefix=`pwd`/../mpich \
+          --enable-static=false \
+          --enable-alloca=true \
+          --disable-long-double \
+          --enable-threads=single \
+          --enable-fortran=no \
+          --enable-fast=all \
+          --enable-g=none \
+          --enable-timing=none
+  make -j4
+  make install
+  cd -
+  rm -rf mpich-3.4.1
 fi
