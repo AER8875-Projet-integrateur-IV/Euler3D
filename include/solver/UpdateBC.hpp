f834f6386323da40604df6b7958e12b07c312ea3
alexandrenoel1004 <alexandre.noel@polymtl.ca>
2021-02-18 12:57:14 -0500

    Started Supersonic Inflow

--- include/solver/UpdateBC.hpp
+++ include/solver/UpdateBC.hpp
@@ -1,9 +1,19 @@
 #pragma once
+#include "solver/FlowField.hpp"
+#include "parser/SimConfig.hpp"
+#include <cmath>
 
 namespace E3D::Solver {
     
     int FindGhostsCells(int elemID, int nbFace);    // Looping over the faces of the element and finding the ghosts cells
 
+    // Whole process of updating BC
+    void UpdateProcess(int elemID);
+
+    // Farfield Supersonic Inflow
+    void FarfieldSupersonicInflow(E3D::Parser::SimConfig &sim);
+
     // Number of ghost cells around one element
+    FlowField Fc;
     int nbGhostCells = 0;
 }
--- src/solver/UpdateBC.cpp
+++ src/solver/UpdateBC.cpp
@@ -5,7 +5,6 @@ using namespace E3D::Solver;
     
     // Function to find the number of ghost cells around on element
         // Needs the element's ID 
-
 int FindGhostCells(int elemID, int nbFace){
     for (int i = 0; i < nbFace; i++){
         if (i=1)
@@ -16,3 +15,12 @@ int FindGhostCells(int elemID, int nbFace){
     } 
     return nbGhostCells;
 };
+
+void UpdateProcess(int elemID){
+    // Add if condition to know if ghost cell is physical or if needs MPI communication
+}
+
+void FarfieldSupersonicInflow(E3D::Parser::SimConfig &sim){
+    double E = sim.getPressure()/((sim.getGamma()-1)*sim.getDensity())+((sim.getVelocity()*cos(sim.getAoA())*sim.getVelocity()*cos(sim.getAoA())+sim.getVelocity()*sin(sim.getAoA())*sim.getVelocity()*sin(sim.getAoA()))/2);
+    
+}
