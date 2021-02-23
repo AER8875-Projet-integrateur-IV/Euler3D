e7e9d9d9f6d57aa07fc8f35d4ac9fb8f0b453c9d
Aziz Ndiaye <azibdndiaye@gmail.com>
2021-02-22 22:41:26 -0500

    partitionnement compile. Il manque la connectivité interne

--- src/preprocessor.cpp
+++ src/preprocessor.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <memory>
 #include "mesh/Mesh.hpp"
 #include "partition/Partition.hpp"
-#include "utils/Logger.hpp"
 #include "spdlog/logger.h"
 #include "spdlog/stopwatch.h"
+#include "utils/Logger.hpp"
+#include <iostream>
+#include <memory>
 /**
  * @brief Entry point for the Euler3D software
  * @param argc : Number of command line parameters
@@ -13,31 +13,32 @@
 */
 
 int main(int argc, char *argv[]) {
-    
-     
-    if (argc != 2) {
-        std::cerr << "Usage : EES2D_APP <meshFileName.su2> " << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    spdlog::stopwatch globalsw;  
-    E3D::Logger logger("log.txt");
-    auto logObject = E3D::Logger::Getspdlog();  
-    logObject->info("Euler 3D Pre-processor.\n");   
-    
-    std::string fileName = argv[1];
-
-    spdlog::stopwatch meshsw;  
-    E3D::Mesh mesh(fileName);
-
-    mesh.solveConnectivity();
-    logObject->debug("Mesh parser run time {}", meshsw); 
-    
-    int nPart = 5;
-    E3D::Partition::Partition part(&mesh, nPart);
-    part.Write();
 
 
-    logObject->debug("Total run time {}", globalsw);     
-    return 0;
+	if (argc != 2) {
+		std::cerr << "Usage : EES2D_APP <meshFileName.su2> " << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	spdlog::stopwatch globalsw;
+	E3D::Logger logger("log.txt");
+	auto logObject = E3D::Logger::Getspdlog();
+	logObject->info("Euler 3D Pre-processor.\n");
+
+	std::string fileName = argv[1];
+
+	spdlog::stopwatch meshsw;
+	E3D::Mesh mesh(fileName);
+
+	mesh.solveConnectivity();
+	logObject->debug("Mesh parser run time {}", meshsw);
+
+	int nPart = 5;
+	std::string SU2OuputPath = "mesh_partition_#.su2";
+	E3D::Partition::Partition part(&mesh, nPart);
+	part.Write(SU2OuputPath);
+
+
+	logObject->debug("Total run time {}", globalsw);
+	return 0;
 }
