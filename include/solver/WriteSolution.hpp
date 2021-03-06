bb95fe25dee53c70f2d2411f8530404704ffe2bd
Aziz Ndiaye <azibdndiaye@gmail.com>
2021-03-06 00:32:05 -0500

    Initialisation de l'écriture des fichiers solutions

---
+++ include/solver/WriteSolution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "mesh/Mesh.hpp"
+#include "parallelization/MPIHandler.hpp"
+#include "parser/SimConfig.hpp"
+#include "solver/ConservativeVar.hpp"
+#include "solver/FlowField.hpp"
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace E3D::Solver {
+
+	class WriteSolution {
+
+	public:
+		WriteSolution(const E3D::Solver::FlowField &flowField,
+		              const E3D::Parser::SimConfig &config,
+		              const E3D::Parallel::MPIHandler &e3d_mpi);
+		~WriteSolution();
+
+	private:
+		const E3D::Solver::FlowField &_flowField;
+		int _rank;
+		std::string _fileName;
+		std::ofstream _file;
+		void write();
+	};
+}// namespace E3D::Solver
