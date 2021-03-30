bb53fba3e12eacbe0517b384b3a02b44afeff1f2
Amin <rouga2012@hotmail.fr>
2021-03-30 11:07:45 -0400

    MPI first version - 1

--- src/parallelization/MPIHandler.cpp
+++ src/parallelization/MPIHandler.cpp
@@ -109,8 +109,7 @@ void MPIHandler::updateFlowField(E3D::Solver::FlowField &localFlowField) const {
                             VarToSend.push_back(localFlowField.GetE()[GhostCell.getGhostCellID()]);
                         }
                         // Recv Buffer, contains variables in this order : [ rho's, u's, v's, w's , E's ]
-                        std::vector<double> VarToReceive;
-                        VarToReceive.resize(VecSize, 0);
+                        std::vector<double> VarToReceive(VecSize);
 
                         MPI_Sendrecv(&VarToSend[0],
                                      VecSize,
@@ -197,8 +196,8 @@ void MPIHandler::updateFlowField(E3D::Solver::FlowField &localFlowField) const {
                             VarToSend.push_back(localFlowField.GetE()[GhostCell.getGhostCellID()]);
                         }
                         // Recv Buffer, contains variables in this order : [ rho's, u's, v's, w's , E's ]
-                        std::vector<double> VarToReceive;
-                        VarToReceive.resize(VecSize, 0.0);
+                        std::vector<double> VarToReceive(VecSize);
+
 
                         MPI_Sendrecv(&VarToSend[0],
                                      VecSize,
